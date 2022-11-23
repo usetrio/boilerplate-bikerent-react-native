261ab89a1038e53d19553c1ceb68af684a0b5927
Carlos Knopel <carlos.knopel@gmail.com>
2022-11-22 22:34:42 -0300

    fix: Setup tests and fix issues when trying to run them

---
+++ test/__mocks__/react-native-reanimated.ts
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler/jestSetup';
+
+declare var global: any;
+
+// eslint-disable-next-line no-underscore-dangle
+global.__reanimatedWorkletInit = jest.fn();
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native-reanimated', () => {
+  // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+  Reanimated.interpolateColor = jest.fn(() => 'mock_color');
+
+  return Reanimated;
+});
